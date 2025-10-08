f60df70502300af7c94535c4781d88128adb4cf6
emmaqqu <emma28q@gmail.com>
2025-10-08 10:57:59 -0400

    telemarketer

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,1 +1,21 @@
 # Lesson 10
+def is_telemarketer(first, second, third, fourth):
+    if first == 8 or first == 9:
+        if fourth == 8 or fourth == 9:
+            if second == third:
+                return True
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
+
+first = int(input("Enter the first digit: "))
+second = int(input("Enter the second digit: "))
+third = int(input("Enter the third digit: "))
+fourth = int(input("Enter the fourth digit: "))
+if is_telemarketer(first, second, third, fourth):
+    print("ignore")
+else:
+    print("answer")
