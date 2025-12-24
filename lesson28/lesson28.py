a8df0f039ebcccab70297e365f584f4e6b073274
emmaqqu <emma28q@gmail.com>
2025-12-23 20:49:27 -0500

    finalized solution

--- lesson28/lesson28.py
+++ lesson28/lesson28.py
@@ -1,1 +1,13 @@
 # Lesson 28
+
+def palindrome(word):
+    i = 0
+    j = len(word) - 1
+    while i < j:
+        if word[i] != word[j]:
+            return False
+        i += 1
+        j -= 1
+    return True
+
+print(palindrome("tacocat"))
