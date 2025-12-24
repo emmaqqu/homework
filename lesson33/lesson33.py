23934755b8d04ca3407901b2a17bee5ad4d6f940
emmaqqu <emma28q@gmail.com>
2025-12-23 20:51:23 -0500

    finalized solution

--- lesson33/lesson33.py
+++ lesson33/lesson33.py
@@ -1,1 +1,14 @@
 # Lesson 33
+
+def centraltendency(listy):
+    freq = len(listy) 
+    mean = sum(listy) / freq
+    sortedver = sorted(listy)
+    mid = freq // 2
+    if freq % 2 == 0:
+        median = (listy[mid] + listy[mid - 1]) / 2
+    else:
+        median = sortedver[mid]
+    return mean, median
+
+print(centraltendency([3, 4, 5, 1, 2]))
