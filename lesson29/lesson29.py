060c3fbf928ac64d8d65764913b8feb3debbd735
emmaqqu <emma28q@gmail.com>
2025-12-23 20:49:38 -0500

    finalized solution

--- lesson29/lesson29.py
+++ lesson29/lesson29.py
@@ -1,1 +1,11 @@
 # Lesson 29
+def consonants(word):
+    vowels = "aeiouy"
+    returned = ""
+    word = word.lower().replace(" ", "")
+    for character in word:
+        if character.isalpha() and character not in vowels:
+            returned += character
+    return returned
+
+print(consonants("i want to eat"))
