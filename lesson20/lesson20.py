be43994a3ac1af8e624b3aa8bc2afc2f4496c0fc
emmaqqu <emma28q@gmail.com>
2025-12-23 20:47:49 -0500

    finalized solution

--- lesson20/lesson20.py
+++ lesson20/lesson20.py
@@ -1,1 +1,11 @@
 # Lesson 20
+def perfect(num):
+    total_sum = 0
+    for num in range(1, 10000):
+        factor_sum = 0
+        for divider in range(1, num):
+            if num % divider == 0:
+                factor_sum += divider
+        if factor_sum == num:
+            total_sum += num
+            return True
