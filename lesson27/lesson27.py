6a9b43eb5fe2e64377758e334a0dc80f908ee473
emmaqqu <emma28q@gmail.com>
2025-12-23 20:49:17 -0500

    finalized solution

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,10 @@
 # Lesson 27
+
+def alphaonly(word):
+    cleanedver = ""
+    for character in word:
+        if character.isalpha():
+            cleanedver += character
+    return cleanedver
+
+print(alphaonly("iuhasd712"))
