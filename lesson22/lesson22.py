014cf827dacebabeef08ea6a043764b282136247
emmaqqu <emma28q@gmail.com>
2025-12-23 20:48:14 -0500

    finalized solution

--- lesson22/lesson22.py
+++ lesson22/lesson22.py
@@ -1,1 +1,13 @@
 # Lesson 22
+
+def fibonnaci(num):
+    fib_0 = 0
+    fib_1 = 1
+    fib_n = 0 # variable initialization
+
+    for value in range(2, num+1):
+        fib_n = fib_1 + fib_0
+        fib_0 = fib_1
+        fib_2 = fib_n
+        
+    return fib_n
