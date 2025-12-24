8ee8782076b1422c58676d235c864544c3c9ab45
emmaqqu <emma28q@gmail.com>
2025-12-23 20:48:01 -0500

    finalized solution

--- lesson21/lesson21.py
+++ lesson21/lesson21.py
@@ -1,1 +1,14 @@
 # Lesson 21
+
+def mostfactors(num):
+    highest = 0 
+    value = 1
+    for i in range(1, num+1):
+        factorcount = 0
+        for j in range(1, i+1)
+            if value % divider == 0:
+                factorcount += 1
+        if factorcount > highest:
+            highest = factorcount
+            value = i
+            
