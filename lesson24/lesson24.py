7d6d0e9c665c40f4bda402b2795e9637c69f401f
emmaqqu <emma28q@gmail.com>
2025-12-23 20:48:34 -0500

    finalized solution

--- lesson24/lesson24.py
+++ lesson24/lesson24.py
@@ -1,1 +1,16 @@
 # Lesson 24
+
+def namelength()
+    longest_length = 0
+    longest_name = ""
+    loop = True
+    while loop:
+        name = input("Enter name or 'x' to exit")
+        if name == "x":
+            loop = False
+        else:
+            current_length = len(name)
+            if current_length > longest_length:
+                longest_length = current_length
+                longest_name = name
+    return longest_name
