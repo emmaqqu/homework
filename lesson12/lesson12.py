02a3159855dd78c25e8d21495dc584eb7d63fd5a
emmaqqu <emma28q@gmail.com>
2025-12-23 20:46:31 -0500

    finalized solution

--- lesson12/lesson12.py
+++ lesson12/lesson12.py
@@ -1,1 +1,13 @@
 # Lesson 12
+
+def triangletype(x, y, z):
+    if x + y + z != 180:
+        print("not a triangle")
+    elif x == 60 and y == 60:
+        print("equilateral")
+    elif x != y and x != z and y != z:
+        print("scalene")
+    else:
+        print("isoceles")
+
+triangletype(40, 60, 80)
