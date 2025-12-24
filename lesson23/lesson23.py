f876ed51510af9de67a8181995b3a54a45491af5
emmaqqu <emma28q@gmail.com>
2025-12-23 20:48:24 -0500

    finalized solution

--- lesson23/lesson23.py
+++ lesson23/lesson23.py
@@ -1,1 +1,19 @@
 # Lesson 23
+
+def average():
+    flag = True
+    total = 0
+    counter = 0
+
+    while flag:
+        mark = input("Enter mark or 'exit'")
+        if mark == "exit":
+            flag = False
+        else:
+            grade = int(mark)
+            if grade >= 0 and grade <= 100:
+                total += grade
+                counter += 1
+    
+    avg = total / counter 
+    return avg
