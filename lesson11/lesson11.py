deeaec32759dafe2ab66ae81776185a8827e693d
emmaqqu <emma28q@gmail.com>
2025-12-23 20:46:21 -0500

    finalized solution

--- lesson11/lesson11.py
+++ lesson11/lesson11.py
@@ -1,1 +1,13 @@
 # Lesson 11
+
+def quadrants(x, y):
+    if x > 0 and y > 0:
+        print("Quadrant 1")
+    elif x > 0 and y < 0: 
+        print("Quadrant 4")
+    elif x < 0 and y > 0:
+        print("Quadrant 2")
+    else:
+        print("Quadrant 3")
+
+quadrants(5, -2)
