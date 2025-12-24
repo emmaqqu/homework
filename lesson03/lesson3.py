0789877d62f7669916715453e39d20a54ed3c192
emmaqqu <emma28q@gmail.com>
2025-12-23 20:44:45 -0500

    finalized solution

--- lesson03/lesson3.py
+++ lesson03/lesson3.py
@@ -1,1 +1,9 @@
 # Lesson 3
+# answer = tiles ** 0.5
+
+tiles = int(input("Enter the # of tiles: "))
+
+from math import sqrt, floor
+answer = floor(sqrt(tiles))
+
+print(f" The side length of the square is {answer}")
