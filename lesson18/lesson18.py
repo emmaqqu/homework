91811be06fa4e5613b879e1f96e394f6dc00710d
emmaqqu <emma28q@gmail.com>
2025-12-23 20:47:30 -0500

    finalized solution

--- lesson18/lesson18.py
+++ lesson18/lesson18.py
@@ -1,1 +1,16 @@
 # Lesson 18
+def factorsyay(num):
+    import math
+    start = 1
+    new_stop = int(math.sqrt(num)) + 1
+    factor_count = 0
+
+    while start < new_stop:
+        if num % start == 0:
+            dividend = num // start
+            if start != dividend:
+                factor_count += 2
+               # start and dividend are factors of num
+            else:
+                factor_count += 1
+        start += 1
