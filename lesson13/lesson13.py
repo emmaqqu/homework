37d13a04e466969d840f5d8cfab05d9101d9355e
emmaqqu <emma28q@gmail.com>
2025-12-23 20:46:48 -0500

    finalized solution

--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -1,1 +1,17 @@
 # Lesson 13
+def specialornah():
+    month = int(input(""))
+    day = int(input(""))
+    if month in range(1, 13):
+        if month == 2 and day == 18:
+            print("Special")
+        elif month == 2 and day < 18:
+            print("Before")
+        elif month == 2 and day > 18:
+            print("After")
+        elif month < 2:
+            print("Before")
+        else:
+            print("After")
+
+specialornah()
