6c104bbca254c801fad2d17d89e795c30c7989a5
emmaqqu <emma28q@gmail.com>
2025-12-23 20:47:40 -0500

    finalized solution

--- lesson19/lesson19.py
+++ lesson19/lesson19.py
@@ -1,1 +1,11 @@
 # Lesson 19
+def isprime(num):
+    i = num - 1
+    while i > 1:
+        if num <= 1:
+            return False
+        elif num % i == 0:
+            return False
+        else:
+            i -= 1
+    return True
