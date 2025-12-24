66497f575d9654777090a3c61c6cbace04a1dd44
emmaqqu <emma28q@gmail.com>
2025-12-23 20:47:02 -0500

    finalized solution

--- lesson16/lesson16.py
+++ lesson16/lesson16.py
@@ -1,1 +1,14 @@
 # Lesson 16
+
+def fizzbuzz():
+    for val in range(1, 51):
+        if val % 3 == 0 and val % 5 == 0:
+            print("FizzBuzz")
+        elif val % 3 == 0:
+            print("Fizz")
+        elif val % 5 == 0:
+            print("Buzz")
+        else:
+            print(val)
+
+fizzbuzz()
