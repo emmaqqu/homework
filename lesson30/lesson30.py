42ed18d60cdc0395ac45da4da5d1912b411a5543
emmaqqu <emma28q@gmail.com>
2025-12-23 20:49:51 -0500

    finalized solution

--- lesson30/lesson30.py
+++ lesson30/lesson30.py
@@ -1,1 +1,11 @@
 # Lesson 30
+def pattern(num):
+    patterns = ""
+    for line in range(1, num+1):
+        if line % 2 == 0: 
+            patterns += "0"
+        else:
+            patterns += "1"
+        print(patterns)
+
+pattern(5)
